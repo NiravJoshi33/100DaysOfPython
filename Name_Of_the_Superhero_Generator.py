34c815f67bfe92f9b8832243c52cbbb29901615a
Nirav_J <niravjoshi3000@gmail.com>
2023-07-09 17:16:18 +0530

    Combined print and input function in one line one time

---
+++ Name_Of_the_Superhero_Generator.py
@@ -0,0 +1,7 @@
+print("Enter the name of your city: ")
+city = input()
+print("Enter the last thing you ate/drank:")
+dish = input()
+print("Enter the name of your favourite animal:")
+animal = input()
+print("Your superhero name is: ", city, " ", dish, " ", animal) 
