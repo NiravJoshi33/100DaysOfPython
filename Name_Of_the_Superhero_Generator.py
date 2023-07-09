a92067b15d318644d4f7e9c170dce6ea2ed7aa89
Nirav_J <niravjoshi3000@gmail.com>
2023-07-09 17:19:51 +0530

    Less lines of code

--- Name_Of_the_Superhero_Generator.py
+++ Name_Of_the_Superhero_Generator.py
@@ -1,7 +1,11 @@
-print("Enter the name of your city: ")
-city = input()
-print("Enter the last thing you ate/drank:")
-dish = input()
-print("Enter the name of your favourite animal:")
-animal = input()
-print("Your superhero name is: ", city, " ", dish, " ", animal) 
+#print("Enter the name of your city:")
+city = input("Enter the name of your city: ") #Instead of printing the prompt first and taking the input, it can be done in one line like this as well. 
+
+#print("Enter the last thing you ate/drank:")
+dish = input("Enter the last thing you ate/drank:")
+
+#print("Enter the name of your favourite animal:")
+animal = input("Enter the name of your favourite animal:")
+
+#print("Your superhero name is: ", city, " ", dish, " ", animal)
+print(city + " " + dish + " " + animal) #alternatively + can be used to concate strings
