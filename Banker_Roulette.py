71190783f7fadfbeeb57465e2238230396b36afd
Nirav_J <niravjoshi3000@gmail.com>
2023-07-13 06:01:45 +0530

    corrected the range of list

--- Banker_Roulette.py
+++ Banker_Roulette.py
@@ -8,7 +8,7 @@ names = n_string.split(", ") #split command splits the string by the mentioned c
 
 #find a random name
 no_of_people = len(names)   #find the no. of elements inside the list
-random_number = random.randint(1,no_of_people-1)    #generate a random number between 1 and no. of elements
+random_number = random.randint(0,no_of_people-1)    #generate a random number between 0 and no. of elements
 payer = names[random_number]    #find a name associated with the random number
 
 print(payer + " will pay the bill today!")
