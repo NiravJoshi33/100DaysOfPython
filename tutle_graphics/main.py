e38af872025df3f2905eb7e9a6b3795aad1004c0
NiravJoshi33 <niravjoshi3000@gmail.com>
2023-07-27 06:46:53 +0530

    program to make square with turtle

---
+++ tutle_graphics/main.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+import random as r
+
+timmy = Turtle("turtle")
+timmy.color("red","green")
+step = r.choice(0,101)
+
+for _ in range(4):
+    timmy.forward(step)
+    timmy.left(step)
+
+
+
+screen = Screen()
+screen.exitonclick()
